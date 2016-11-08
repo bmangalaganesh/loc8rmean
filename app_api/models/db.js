c75749041f52a9d99f06b5bd9d438deac870b535
manglu <manglu@gmail.com>
2016-11-08 12:43:00 +1100

    externalise the dbURI to pick it from cfenv (VCAP Services). It was hardcoded earlier to localhost

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,7 +1,31 @@
 var mongoose = require('mongoose');
 require('./locations');
+var cfenv = require('cfenv');
 
-var dbURI = 'mongodb://localhost/Loc8r';
+
+//get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+// set the default value to islocal (i.e app is running locally) 
+var isLocal = true; 
+var dbURI = 'mongodb://localhost/Loc8r';//set the DB to the local database
+
+if (appEnv) {
+	// If appEnv is not null then print the object so that we can see what is in
+	// it...
+	console.log("App Env is:" + JSON.stringify(appEnv));
+	console.log("is the app running locally:" + isLocal);
+}
+
+if (!isLocal){
+	//If it is a remote execution (i.e Bluemix use the information from the VCAP Services...
+	
+	var serviceName = "compose-for-mongodb";
+	dbURI = appEnv.services[serviceName] ? appEnv.services[serviceName][0].credentials.uri: ""; 
+}
+
+console.log("dbURI is:"+ dbURI);
+
 mongoose.connect(dbURI);
 
 mongoose.connection.on('connected', function() {
